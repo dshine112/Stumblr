2cc5f257c9339d8187b3faa6c53b5a88e437fadc
Dylan Shine <dylanshinenyc@gmail.com>
2015-01-07 10:23:00 -0500

    Added additional logic to automatically fetch new content for category every day

--- app/controllers/search.rb
+++ app/controllers/search.rb
@@ -3,6 +3,13 @@ post '/search' do
   if @category
     @category.searchCount += 1
     @category.save
+    if @category.created_at.to_date < (Time.now.to_date - 1)
+      @category.deleteContent
+      @category.createContent(@category.search)
+      @category.updateThumbnail
+      @category.update(created_at: Time.now)
+      @category.save
+    end
   else
     @category = Category.create(name: params[:search].downcase, searchCount: 1)
     @category.updateThumbnail
