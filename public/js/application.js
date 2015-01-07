c08eaaa859f012b7bd4fd3128ba214a3a9076438
Dylan Shine <dylanshinenyc@gmail.com>
2015-01-07 10:31:42 -0500

    Added youtubeId helper function to parse YouTube video id from Video uri

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
+
+function youtubeId(url) {
+  var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+  var match = url.match(regExp);
+
+  if (match && match[2].length == 11) {
+    return "//www.youtube.com/embed/" + match[2];
+  }
+}
