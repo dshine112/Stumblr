ef023205c788b7a9e7e2a262bd9c64367cad4460
Dylan Shine <dylanshinenyc@gmail.com>
2015-01-07 10:40:21 -0500

    Added AJAX call for initial search by User using the thumbnail images

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  //AJAX call for initial search
+  //AJAX call for initial search with search bar
   $('#search').on('submit', function(event) {
     event.preventDefault();
     $("#search").hide();
@@ -16,6 +16,37 @@ $(document).ready(function() {
     }).done(function(response) {
       content = response
       $("#initial").remove();
+      $("#searchWrapper img").hide();
+      $("#search").show();
+      $("#likeButtons").show();
+      $("#content").show();
+      $content = $("#content iframe")
+      contentNum = 0;
+      if (content[contentNum].type === "video") {
+        $content.attr("src", youtubeId(content[contentNum].url))
+        $content.attr("contentId", content[contentNum].id)
+      } else {
+        $content.attr("src", content[contentNum].url)
+        $content.attr("contentId", content[contentNum].id)
+      }
+    })
+  })
+
+  //AJAX call for initial search with thumbnail images
+  $('.thumbnailSearch').on('click', function(event) {
+    event.preventDefault();
+    $("#search").hide();
+    $("#initial").show();
+    $(".homepage").hide();
+    $("#searchWrapper img").show();
+    var $target = $(event.target);
+    $.ajax({
+      url: '/search/' + $target.attr('categoryId'),
+      type: 'GET',
+      dataType: "JSON"
+    }).done(function(response) {
+      content = response
+      $("#initial").hide();
       $("#searchWrapper img").hide();
       $("#search").show();
       $("#likeButtons").show();
