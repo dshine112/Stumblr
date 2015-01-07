cca019d50ac70cff9504507f340f14216f2f7e9a
Dylan Shine <dylanshinenyc@gmail.com>
2015-01-07 09:58:01 -0500

    Added search method to Category model, get content URIs from Google API

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,18 @@ class Category < ActiveRecord::Base
   has_many :pictures
   has_many :videos
   has_many :articles
+
+  def search
+    videos = []
+    articles = []
+    Google::Search::Video.new(:query => self.name, :size => :small).each do |video|
+      videos << [video.uri, video.title]
+    end
+
+    Google::Search::News.new(:query => self.name, :edition => :us).each do |article|
+      articles << [article.uri, article.title]
+    end
+    return [videos, articles, self.id]
+  end
+
 end
