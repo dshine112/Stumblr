ab1f3047b363226ab54f945629b048444aca558a
Dylan Shine <dylanshinenyc@gmail.com>
2015-01-07 10:35:53 -0500

    Added AJAX call for initial search by User using the search bar

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
 
+  //AJAX call for initial search
+  $('#search').on('submit', function(event) {
+    event.preventDefault();
+    $("#search").hide();
+    $("#initial").show();
+    $(".homepage").hide();
+    $("#searchWrapper img").show();
+    var $target = $(event.target);
+    $.ajax({
+      url: $target.attr('action'),
+      type: 'POST',
+      data: $target.serialize(),
+      dataType: "JSON"
+    }).done(function(response) {
+      content = response
+      $("#initial").remove();
+      $("#searchWrapper img").hide();
+      $("#search").show();
+      $("#likeButtons").show();
+      $("#content").show();
+      $content = $("#content iframe")
+      contentNum = 0;
+      if (content[contentNum].type === "video") {
+        $content.attr("src", youtubeId(content[contentNum].url))
+        $content.attr("contentId", content[contentNum].id)
+      } else {
+        $content.attr("src", content[contentNum].url)
+        $content.attr("contentId", content[contentNum].id)
+      }
+    })
+  })
+
 });
 
 function youtubeId(url) {
