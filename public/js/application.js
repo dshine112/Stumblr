63f8d337483feb016318e3406cc1317abf7d6948
Dylan Shine <dylanshinenyc@gmail.com>
2015-01-07 16:32:02 -0500

    Switched gif for JS animated sprite

--- public/js/application.js
+++ public/js/application.js
@@ -137,6 +137,9 @@ $(document).ready(function() {
     plugins: [ListFuzzySearch()]
   });
 
+  //The following code starts the animation
+  new imageLoader(cImageSrc, 'startAnimation()');
+
 });
 
 function youtubeId(url) {
@@ -172,13 +175,13 @@ function searchSetup() {
   $("#search").hide();
   $("#initial").show();
   $(".homepage").hide();
-  $("#searchWrapper img").show();
+  $("#searchWrapper #loaderImage").show();
 }
 
 function searchFinish(response) {
   content = response
   $("#initial").remove();
-  $("#searchWrapper img").hide();
+  $("#searchWrapper #loaderImage").hide();
   $("#search").show();
   $("#likeButtons").show();
   $("#content").show();
@@ -202,3 +205,66 @@ function searchFinish(response) {
   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+
+var cSpeed = 9;
+var cWidth = 256;
+var cHeight = 88;
+var cTotalFrames = 11;
+var cFrameWidth = 256;
+var cImageSrc = '../img/sprites.png';
+
+var cImageTimeout = false;
+var cIndex = 0;
+var cXpos = 0;
+var cPreloaderTimeout = false;
+var SECONDS_BETWEEN_FRAMES = 0;
+
+function startAnimation() {
+
+  document.getElementById('loaderImage').style.backgroundImage = 'url(' + cImageSrc + ')';
+  document.getElementById('loaderImage').style.width = cWidth + 'px';
+  document.getElementById('loaderImage').style.height = cHeight + 'px';
+
+  //FPS = Math.round(100/(maxSpeed+2-speed));
+  FPS = Math.round(100 / cSpeed);
+  SECONDS_BETWEEN_FRAMES = 1 / FPS;
+
+  cPreloaderTimeout = setTimeout('continueAnimation()', SECONDS_BETWEEN_FRAMES / 1000);
+
+}
+
+function continueAnimation() {
+
+  cXpos += cFrameWidth;
+  //increase the index so we know which frame of our animation we are currently on
+  cIndex += 1;
+
+  //if our cIndex is higher than our total number of frames, we're at the end and should restart
+  if (cIndex >= cTotalFrames) {
+    cXpos = 0;
+    cIndex = 0;
+  }
+
+  if (document.getElementById('loaderImage'))
+    document.getElementById('loaderImage').style.backgroundPosition = (-cXpos) + 'px 0';
+
+  cPreloaderTimeout = setTimeout('continueAnimation()', SECONDS_BETWEEN_FRAMES * 1000);
+}
+
+function stopAnimation() { //stops animation
+  clearTimeout(cPreloaderTimeout);
+  cPreloaderTimeout = false;
+}
+
+function imageLoader(s, fun) //Pre-loads the sprites image
+{
+  clearTimeout(cImageTimeout);
+  cImageTimeout = 0;
+  genImage = new Image();
+  genImage.onload = function() {
+    cImageTimeout = setTimeout(fun, 0)
+  };
+  genImage.onerror = new Function('alert(\'Could not load the image\')');
+  genImage.src = s;
+}
