a1d1058e11b6fe95978094464f21bdbdd2d004b9
Dylan Shine <dylanshinenyc@gmail.com>
2015-01-07 10:47:06 -0500

    Added route for User to like Article

--- app/controllers/like.rb
+++ app/controllers/like.rb
@@ -8,3 +8,14 @@ post '/video/:id' do |id|
     "exists"
   end
 end
+
+post '/article/:id' do |id|
+  article = Article.find(id)
+  if article.likes.find_by(user_id: session[:user_id]) == nil
+    article.likeCount += 1
+    article.save
+    Like.create(user_id: session[:user_id], article_id: article.id, uri: article.uri, title: video.title)
+  else
+  "exists"
+  end
+end
