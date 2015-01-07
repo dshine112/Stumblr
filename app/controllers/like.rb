5760f1c8c88d900cdd72392b588f386ff638a15d
Dylan Shine <dylanshinenyc@gmail.com>
2015-01-07 12:06:54 -0500

    Fixed typo in POST article route

--- app/controllers/like.rb
+++ app/controllers/like.rb
@@ -1,3 +1,14 @@
+post '/article/:id' do |id|
+  article = Article.find(id)
+  if article.likes.find_by(user_id: session[:user_id]) == nil
+    article.likeCount += 1
+    article.save
+    Like.create(user_id: session[:user_id], article_id: article.id, uri: article.uri, title: article.title)
+  else
+  "exists"
+  end
+end
+
 post '/video/:id' do |id|
   video = Video.find(id)
   if video.likes.find_by(user_id: session[:user_id]) == nil
@@ -6,17 +17,6 @@ post '/video/:id' do |id|
     Like.create(user_id: session[:user_id], video_id: video.id, uri: video.uri, title: video.title)
   else
     "exists"
-  end
-end
-
-post '/article/:id' do |id|
-  article = Article.find(id)
-  if article.likes.find_by(user_id: session[:user_id]) == nil
-    article.likeCount += 1
-    article.save
-    Like.create(user_id: session[:user_id], article_id: article.id, uri: article.uri, title: video.title)
-  else
-  "exists"
   end
 end
 
